6f3465ff6cf433fd86fa749b0282d98f8a209295
Unknown <thomaspoint@gmail.com>
2017-12-12 13:41:41 +0100

    Da

---
+++ test/src/list.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default class List extends React.Component {
+    render() {
+        return (
+            <ul>
+                {this.props.items.map((item, index) => <li key={index}>{item}</li>)}
+            </ul>
+        )
+    }
+}
+
