b1bca2a2de297dd8987281f73a91673ae538854a
Unknown <thomaspoint@gmail.com>
2017-12-20 12:19:09 +0100

    dada

--- openclassroom/src/Components/SearchBar.js
+++ openclassroom/src/Components/SearchBar.js
@@ -1,9 +1,23 @@
 import React from 'react';
 
 export default class SearchBar extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            term: ''
+        };
+    }
+
+    onInputChange(event) {
+        let target = event.target.value;
+        this.setState({term: target});
+    }
+
     render() {
         return (
-            <input/>
-        )
+            <div>
+                <input value = {this.state.term} onChange={this.onInputChange.bind(this)}/>
+            </div>
+        );
     }
 }
