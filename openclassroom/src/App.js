95e35ae5112cc8b7073da3f7a6f75b467d61a363
Unknown <thomaspoint@gmail.com>
2017-12-20 14:14:21 +0100

    Api_key YT

--- openclassroom/src/App.js
+++ openclassroom/src/App.js
@@ -2,6 +2,13 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import SearchBar from "./Components/SearchBar";
+import YTSearch from "youtube-api-search";
+
+const API_KEY = 'AIzaSyDF3Ay61LLhmXeI0BU_-AEajy0hEhnoVVg';
+
+YTSearch({key: API_KEY, term:'surfboards'}, function(data){
+    console.log(data);
+})
 
 class App extends Component {
   render() {
